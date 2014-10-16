b0de5c31a1905d822384fee7213d7fbf20a653d4
Prashanth Adhikari <prashanth66@yahoo.com>
2014-10-15 21:57:04 -0700

    Added login page.

--- static/app.js
+++ static/app.js
@@ -7,10 +7,18 @@
                 templateUrl:'home.html',
                 controller: 'homeController'
             })
+            .when('/login', {
+                templateUrl:'login.html',
+                controller: 'loginController'
+            })
     });
 
     app.controller("homeController", function($scope,$http){
         $http.get("/concur/api/home")
             .success(function(response) {$scope.home = response;});
+    });
+
+    app.controller("loginController", function($scope,$http){
+        $scope.message = "Hello Login";
     });
 
